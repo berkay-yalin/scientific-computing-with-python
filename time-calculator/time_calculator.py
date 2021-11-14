e1cfff940b4030230fdbe601e7115471dfb260dc
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2021-11-14 00:11:53 +0000

    Update time_calculator.py

--- time-calculator/time_calculator.py
+++ time-calculator/time_calculator.py
@@ -1,7 +1,36 @@
-def add_time(start, duration):
+from datetime import datetime
+from datetime import timedelta
+from calendar import day_name
 
+def add_time(*args):
+    if len(args) == 2:
+        info = '0001 01 01 {}'.format(args[0])
 
+        start = datetime.strptime(info,'%Y %m %d %I:%M %p')
+        stop = timedelta(hours   = int(args[1].split(':')[0]),
+                         minutes = int(args[1].split(':')[1])) + start
 
+        endDate = stop.strftime('%I:%M %p')
+        if endDate[0] == '0': endDate = endDate[1:]
+        difference = stop.day - start.day
 
+        if difference == 0: return endDate
+        elif difference == 1: return '{} (next day)'.format(endDate)
+        else: return '{} ({} days later)'.format(endDate, difference)
 
-    return new_time
+    if len(args) == 3:
+        day = list(day_name).index(args[2].title()) + 1
+        info = '0001 01 0{} {}'.format(day, args[0])
+
+        start = datetime.strptime(info,'%Y %m %d %I:%M %p')
+        stop = timedelta(hours   = int(args[1].split(':')[0]),
+                         minutes = int(args[1].split(':')[1])) + start
+
+        endDate = stop.strftime('%I:%M %p')
+        if endDate[0] == '0': endDate = endDate[1:]
+        weekday = stop.strftime('%A')
+        difference = stop.day - start.day
+
+        if difference == 0: return '{}, {}'.format(endDate, weekday)
+        elif difference == 1: return '{}, {} (next day)'.format(endDate, weekday)
+        else: return '{}, {} ({} days later)'.format(endDate, weekday, difference)
