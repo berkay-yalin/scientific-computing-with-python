fc6890f855c50d519581036d640c48507a03b6e5
berkay <66202981+berkay-yalin@users.noreply.github.com>
2022-06-01 18:16:01 +0100

    Update prob_calculator.py

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -3,6 +3,31 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        contents = [[k for i in range(int(v))] for k, v in kwargs.items()]
+        self.contents = [j for i in contents for j in i]
 
+    def draw(self, draws):
+        if draws >= len(self.contents):
+            return self.contents
+        else:
+            drawn = []
+            for _ in range(draws):
+                index = random.randrange(len(self.contents))
+                drawn.append(self.contents[index])
+                self.contents.pop(index)
+            return drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    successes = 0
+    for i in range(num_experiments):
+        balls = list(set(hat.contents))
+        events = copy.deepcopy(hat).draw(num_balls_drawn)
+
+        observed = {i: events.count(i) for i in balls if i in expected_balls}
+        expected = expected_balls
+
+        if all(observed[i] >= expected[i] for i in observed):
+            successes += 1
+
+    return successes/num_experiments
