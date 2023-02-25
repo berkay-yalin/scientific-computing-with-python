6a7afec88581d1671ae52a2cb95cc2601b094f44
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-02-25 23:24:57 +0000

    Update arithmetic_arranger.py

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -1,42 +1,30 @@
 import operator
-from re import findall
+ops = {'+': operator.add, '-': operator.sub}
 
 def arithmetic_arranger(problems, display = False):
-    def validateEquations(operators, operands, other):
-        if len(problems) > 5:
-            return "Error: Too many problems."
-        elif any(not i for i in operators):
+    DATA = [i.split(' ') for i in problems]
+
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    for num1, sign, num2 in DATA:
+        if sign not in ['+', '-']:
             return "Error: Operator must be '+' or '-'."
-        elif any(i for i in other):
+        if not num1.isnumeric() or not num2.isnumeric():
             return "Error: Numbers must only contain digits."
-        elif any(len(j) > 4 for i in operands for j in i):
+        if len(num1) > 4 or len(num2) > 4:
             return "Error: Numbers cannot be more than four digits."
-        else:
-            return True
+    
+    lines = [[], [], [], []]
+    for num1, sign, num2 in DATA:
+        width = len(max([num1, num2], key=len)) + 2
+        lines[0].append(num1.rjust(width, ' '))
+        lines[1].append(num2.rjust(width, ' ').replace(' ', sign, 1))
+        lines[2].append('-' * width)
+        lines[3].append(str(ops[sign](int(num1),int(num2))).rjust(width, ' '))
+    output = list(map(lambda x: '    '.join(x), lines))
+    
+    if display == True:
+        return '\n'.join(output)
+    else:
+        return '\n'.join(output[:-1])
 
-    def formatEquation(num1, sign, num2):
-        ops = {'+': operator.add, '-': operator.sub}
-        length = len(max([num1, num2], key = len)) + 1 + 1
-        return [
-            num1.rjust(length, ' '),
-            num2.rjust(length, ' ').replace(' ', sign, 1),
-            '-' * length,
-            str(ops[sign](int(num1),int(num2))).rjust(length, ' ')
-        ]
-
-    def displayEquations(display):
-        array = [formatEquation(*i.split(' ')) for i in problems]
-        array = ['    '.join(i) for i in [list(i) for i in zip(*array)]]
-
-        if display == True:
-            return '\n'.join(array)
-        else:
-            return '\n'.join(array[:len(array) - 1])
-
-    ### PARENT FUNCTION ###
-    test = validateEquations(
-        list(map(lambda x: findall(r'[+-]', x), problems)),
-        list(map(lambda x: findall(r'\d+', x), problems)),
-        list(map(lambda x: findall(r'[^\d\s+-]', x), problems))
-    )
-    return displayEquations(display) if test == True else test
