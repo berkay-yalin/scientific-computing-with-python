3c49c5e133d48673480b35156477e04a3290fbff
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-05-12 19:26:28 +0100

    Update arithmetic_arranger.py

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -1,30 +1,25 @@
-import operator
-ops = {'+': operator.add, '-': operator.sub}
-
 def arithmetic_arranger(problems, display = False):
     DATA = [i.split(' ') for i in problems]
 
     if len(problems) > 5:
         return "Error: Too many problems."
     for num1, sign, num2 in DATA:
-        if sign not in ['+', '-']:
+        if sign not in ('+', '-'):
             return "Error: Operator must be '+' or '-'."
         if not num1.isnumeric() or not num2.isnumeric():
             return "Error: Numbers must only contain digits."
         if len(num1) > 4 or len(num2) > 4:
             return "Error: Numbers cannot be more than four digits."
-    
+
     lines = [[], [], [], []]
     for num1, sign, num2 in DATA:
         width = len(max([num1, num2], key=len)) + 2
-        lines[0].append(num1.rjust(width, ' '))
-        lines[1].append(num2.rjust(width, ' ').replace(' ', sign, 1))
+        result = int(num1) + int(num2) if sign == '+' else int(num1) - int(num2)
+        lines[0].append(num1.rjust(width))
+        lines[1].append(num2.rjust(width).replace(' ', sign, 1))
         lines[2].append('-' * width)
-        lines[3].append(str(ops[sign](int(num1),int(num2))).rjust(width, ' '))
-    output = list(map(lambda x: '    '.join(x), lines))
-    
-    if display == True:
-        return '\n'.join(output)
-    else:
-        return '\n'.join(output[:-1])
+        lines[3].append(str(result).rjust(width))
+
+    output = ['    '.join(i) for i in lines]
+    return '\n'.join(output if display else output[:-1])
 
